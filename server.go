aec05c3886bded9aa8e65475ce5bbf662b1e8487
Muhammad Sheryar Butt <muhammad.sheryar@teo-intl.com>
2022-06-21 13:14:03 +0500

    Added basic middleware for Authorization, Logging and Debugging

--- server.go
+++ server.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"io"
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/sheryarbutt/Learn-Go-Gin/controller"
+	"github.com/sheryarbutt/Learn-Go-Gin/middlewares"
 	"github.com/sheryarbutt/Learn-Go-Gin/service"
+	gindump "github.com/tpkeeper/gin-dump"
 )
 
 var (
@@ -11,9 +16,18 @@ var (
 	VideoController controller.VideoController = controller.New(videoService)
 )
 
+func setupLogOuput() {
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+}
+
 func main() {
 
-	server := gin.Default()
+	setupLogOuput()
+	server := gin.New()
+
+	server.Use(gin.Recovery(), middlewares.Logger(), middlewares.BasicAuth(), gindump.Dump())
+
 	server.GET("/videos", func(ctx *gin.Context) {
 		ctx.JSON(200, VideoController.FindAll())
 	})
