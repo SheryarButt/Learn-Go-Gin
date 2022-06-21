9bd0eecdb8be8e909710b187c34f86f48f5a6aee
Muhammad Sheryar Butt <muhammad.sheryar@teo-intl.com>
2022-06-21 14:55:10 +0500

    Added a custom validation for title to include a 'cool' keyword.

--- controller/video-controller.go
+++ controller/video-controller.go
@@ -2,8 +2,10 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 	"github.com/sheryarbutt/Learn-Go-Gin/entity"
 	"github.com/sheryarbutt/Learn-Go-Gin/service"
+	"github.com/sheryarbutt/Learn-Go-Gin/validators"
 )
 
 type VideoController interface {
@@ -15,7 +17,12 @@ type controller struct {
 	service service.VideoService
 }
 
+var validate *validator.Validate
+
 func New(service service.VideoService) VideoController {
+	validate = validator.New()
+	validate.RegisterValidation("is-cool", validators.ValidateCoolTitle)
+
 	return &controller{
 		service: service,
 	}
@@ -29,6 +36,10 @@ func (c *controller) Save(ctx *gin.Context) error {
 
 	video := entity.Video{}
 	err := ctx.ShouldBindJSON(&video)
+	if err != nil {
+		return err
+	}
+	err = validate.Struct(video)
 	if err != nil {
 		return err
 	}
